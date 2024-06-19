b912ef8e959f6a3443729923f81e3bca2311ae82
Yakup <yakupjoraev@gmail.com>
2024-06-19 20:18:59 +0300

    added new videos and banners for hero section

--- src/js/main.js
+++ src/js/main.js
@@ -39,21 +39,6 @@ function burgerMenu() {
 burgerMenu()
 
 
-// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
-function fixedNav() {
-  const nav = document.querySelector('nav')
-
-  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-  const breakpoint = 1
-  if (window.scrollY >= breakpoint) {
-    nav.classList.add('fixed__nav')
-  } else {
-    nav.classList.remove('fixed__nav')
-  }
-}
-window.addEventListener('scroll', fixedNav)
-
-
 function btnAnimation() {
   let btns = document.querySelectorAll('.btn');
 
