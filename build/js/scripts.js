20f8e9054228f7d1760f8e7c404cb777a8345fe7
Yakup <yakupjoraev@gmail.com>
2024-06-20 15:13:16 +0300

    added new event for burger function

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -3,41 +3,53 @@
 
 // Мобильное меню бургер
 function burgerMenu() {
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
-  const body = document.querySelector('body')
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  const body = document.querySelector('body');
+
   burger.addEventListener('click', () => {
     if (!menu.classList.contains('active')) {
-      menu.classList.add('active')
-      burger.classList.add('active-burger')
-      body.classList.add('locked')
+      menu.classList.add('active');
+      burger.classList.add('active-burger');
+      body.classList.add('locked');
     } else {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
     }
-  })
-  //снять классы при клике на элементы меню
-  const menuItems = document.querySelectorAll('.menu__item')
+  });
+
+  // Снять классы при клике на элементы меню
+  const menuItems = document.querySelectorAll('.menu__item');
+  const menuItemBtn = document.querySelectorAll('.menu__item-btn');
 
   menuItems.forEach(item => {
     item.addEventListener('click', function () {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    })
+      if (item.classList.contains('menu__item-btn')) {
+        setTimeout(() => {
+          menu.classList.remove('active');
+          burger.classList.remove('active-burger');
+          body.classList.remove('locked');
+        }, 800);
+      } else {
+        menu.classList.remove('active');
+        burger.classList.remove('active-burger');
+        body.classList.remove('locked');
+      }
+    });
   });
 
   // Вот тут мы ставим брейкпоинт навбара
   window.addEventListener('resize', () => {
     if (window.innerWidth > 991.98) {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
     }
-  })
+  });
 }
-burgerMenu()
+
+burgerMenu();
 
 
 function btnAnimation() {
